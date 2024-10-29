d0f6bdf06c3269a17c68953b19937640e59000bb
Naufalarib <daveexplainbelajar@gmail.com>
2024-10-29 21:32:00 +0700

    animated slider

---
+++ script1.js
@@ -0,0 +1,44 @@
+const slider = document.querySelector(".slider");
+const list = document.querySelector(".list");
+const thumbnail = document.querySelector(".thumbnail");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+// Auto play slider
+let runAutoPlay = setTimeout(() => {
+  next.click();
+}, 8000);
+
+next.addEventListener("click", () => {
+  initSlider("next");
+});
+
+prev.addEventListener("click", () => {
+  initSlider("prev");
+});
+
+const initSlider = (type) => {
+  const sliderItems = list.querySelectorAll(".item");
+  const thumbnailItem = thumbnail.querySelectorAll(".item");
+
+  if (type === "next") {
+    list.appendChild(sliderItems[0]);
+    thumbnail.appendChild(thumbnailItem[0]);
+    slider.classList.add("next");
+  } else {
+    const lastItemPosition = sliderItems.length - 1;
+    list.prepend(sliderItems[lastItemPosition]);
+    thumbnail.prepend(thumbnailItem[lastItemPosition]);
+    slider.classList.add("prev");
+  }
+
+  setTimeout(() => {
+    slider.classList.remove("next");
+    slider.classList.remove("prev");
+  }, 2000);
+
+  clearTimeout(runAutoPlay);
+  runAutoPlay = setTimeout(() => {
+    next.click();
+  }, 8000);
+};
